24838c6206e09a0bebdfbcd91385973b4a2fa8ff
Vandan Sawant <vanss25a@gmail.com>
2020-12-12 13:48:26 +0530

    backend:added endpoints routes

--- server.js
+++ server.js
@@ -1,21 +1,100 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const mongoData = require('./mongoData');
+const cors = require('cors');
 
+// App Config
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req, res) => res.status(200).send('API Running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-const PORT = process.env.PORT || 5000;
+app.post('/new/channel', (req,res) => {
+    const dbData = req.body
+
+    mongoData.create(dbData, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/get/channelList', (req,res) => {
+    mongoData.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            let channels = []
+
+            data.map((channelData) => {
+                const channelInfo = {
+                    id: channelData._id,
+                    name: channelData.channelName
+                }
+                channels.push(channelInfo)
+            })
+
+            res.status(200).send(channels)
+        }
+    })
+})
+
+
+app.post('/new/message', (req, res) => {
+    const newMessage = req.body
+
+    mongoData.update(
+        {_id: req.query.id},
+        {$push: {conversation: req.body}},
+        (err, data) => {
+            if (err) {
+                console.log('Error saving message..')
+                console.log(err)
+                res.status(500).send(err)
+            } else {
+                res.status(201).send(data)
+            }
+        }
+    )
+})
+
+
+app.get('/get/data', (req, res) => {
+    mongoData.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.get('/get/conversation', (req, res) => {
+    const id = req.query.id
+
+    mongoData.find({_id: id}, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
